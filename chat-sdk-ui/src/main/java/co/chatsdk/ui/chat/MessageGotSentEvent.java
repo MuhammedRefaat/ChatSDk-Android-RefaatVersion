ebabae58f22007b248b015991a851d5dc57509c2
Muhammed Refaat <mrefaat.7889@gmail.com>
2019-07-18 11:28:57 +0400

    Adding an EventBus to indicate SentMessage

---
+++ chat-sdk-ui/src/main/java/co/chatsdk/ui/chat/MessageGotSentEvent.java
@@ -0,0 +1,11 @@
+package co.chatsdk.ui.chat;
+
+public class MessageGotSentEvent {
+
+    public boolean success;
+
+    public MessageGotSentEvent(boolean success) {
+        this.success = success;
+    }
+
+}
